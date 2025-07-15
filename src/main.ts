6b9e98257bde5bf059005f15567cd889d1651a99
wdm-coding <wdmcoding0620@163.com>
2025-07-15 15:44:06 +0800

    feat(utils): :sparkles: 开发工具函数

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import '@/styles/index.scss'
 import 'virtual:svg-icons-register'
+import globProvide from '@/utils/tools/index'
 import App from '@/App.vue'
 import router from '@/router'
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(globProvide).mount('#app')
---
+++ src/utils/tools/test.ts
@@ -0,0 +1,32 @@
+// 1.判断连接是否是网络连接
+export function isNetwork(url: string): boolean {
+  const pattern = /^https?:\/\//i // 正则表达式，匹配以http或https开头的URL
+  return pattern.test(url)
+}
+// 2.判断对象是否为空
+export function isEmptyObject(obj: object): boolean {
+  return Object.keys(obj).length === 0 && obj.constructor === Object
+}
+// 3. 判断对象的每个属性是否为空
+export function isEmptyObjectValue(obj: object): boolean {
+  return Object.values(obj).every( value => value === null || value === undefined )
+}
+// 4. 监测对象的属性是否为空，为空则删除这个属性，返回一个新的对象
+export function hasValueObject<T extends Record<string, any>>(obj: T): Partial<T> {
+  const newObj: Partial<T> = {}
+  for (const key in obj) {
+    if (!isEmptyObjectValue(obj[key])) {
+      newObj[key] = obj[key]
+    }
+  }
+  return newObj
+}
+
+export default {
+  isNetwork,
+  isEmptyObject,
+  isEmptyObjectValue,
+  hasValueObject
+}
+
+

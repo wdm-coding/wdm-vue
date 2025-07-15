db4b42e3c3b0b2ac2ca84db5e7f6dfc2a8262b19
wdm-coding <wdmcoding0620@163.com>
2025-07-15 17:51:29 +0800

    ci: :ferris_wheel: 微调

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import '@/styles/index.scss'
 import 'virtual:svg-icons-register'
-import globProvide from '@/utils/tools/index'
 import App from '@/App.vue'
 import router from '@/router'
-createApp(App).use(router).use(globProvide).mount('#app')
+createApp(App).use(router).mount('#app')
